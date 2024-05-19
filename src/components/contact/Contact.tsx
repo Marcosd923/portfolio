a8edd144b095e13691be4af26a09d3f8de1776ea
Marcos923 <marcos.daut@gmail.com>
2024-05-19 17:57:38 -0300

    modificando formulario de contacto

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,11 +4,11 @@ import { Form } from "../form/Form";
 export const Contact = () => {
   return (
     <section id="contacto">
-      <div className="">
+      <div className="flex justify-center">
         <h1
           //   data-aos="fade-down"
           //   data-aos-duration="1000"
-          className="font-bold text-grisclaro text-3xl  mb-10 flex justify-center  before:content-['04.'] before:text-naranja divide-y-0 "
+          className="font-bold text-grisclaro text-3xl  mb-10 w-2/5 ml-20 text-end  before:content-['04.'] before:text-naranja divide-y-0 "
         >
           Contacto
         </h1>
@@ -19,7 +19,7 @@ export const Contact = () => {
         className="  w-full mx-auto  font-sans   max-w-3xl lg:max-w-4xl  px-2 sm:px-4 md:px-6"
       >
         <div className="text-grismedio   sm:max-w-prose md:max-w-xl lg:max-w-3xl text-base lg:text-lg">
-          <p className="text-center">
+          <p className="text-end">
             Actualmente me encuentro en búsqueda de nuevas oportunidades,
             cualquier oferta será bien recibida. Si simplemente deseas hacerme
             una pregunta o saludarme, no dudes en ponerte en contacto conmigo.
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,58 +2,65 @@ import React from "react";
 
 export const Form = () => {
   return (
-    <section className="flex justify-center text-grismedio mt-20 ">
-      <form action="" className="  grid gap-y-0">
-        <label htmlFor="name" className="text-blanco text-lg">
-          Nombre
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Ingrese su nombre"
-          className="rounded-md w-72 mb-2 font-sans pl-2 p-1 text-sm py-2"
-        />
+    <section className="mt-20 ">
+      <div
+        className="grid sm:grid-cols-2 items-center gap-16 p-8 mx-auto max-w-2xl
+        bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md
+        text-[#333] font-[sans-serif]"
+      >
+        <div></div>
+        <form action="" className="  grid gap-y-0">
+          <label htmlFor="name" className="text-blanco text-lg">
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Ingrese su nombre"
+            className="rounded-md  mb-2 font-sans pl-2 p-1 text-sm py-2"
+          />
 
-        <label htmlFor="email" className="text-blanco text-lg">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="rounded-md mb-2 font-sans pl-2 p-1 text-sm py-2"
-          placeholder="Ejemplo@email.com"
-        />
+          <label htmlFor="email" className="text-blanco text-lg">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="rounded-md mb-2 font-sans pl-2 p-1 text-sm py-2"
+            placeholder="Ejemplo@email.com"
+          />
 
-        <label htmlFor="subjet" className=" text-blanco text-lg">
-          Asunto
-        </label>
-        <input
-          type="text"
-          id="subjet"
-          name="subjet"
-          className="rounded-md mb-2 font-sans pl-2 p-0.5 text-sm py-2"
-          placeholder="Coloque un asunto"
-        />
+          <label htmlFor="subjet" className=" text-blanco text-lg">
+            Asunto
+          </label>
+          <input
+            type="text"
+            id="subjet"
+            name="subjet"
+            className="rounded-md mb-2 font-sans pl-2 p-0.5 text-sm py-2"
+            placeholder="Coloque un asunto"
+          />
 
-        <label htmlFor="message" className="text-blanco text-lg">
-          Mensaje
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          className="rounded-md mt-0.5 font-sans pl-2 p-0.5 text-sm h-16"
-          placeholder="Escriba su mensaje aquí"
-        />
+          <label htmlFor="message" className="text-blanco text-lg">
+            Mensaje
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="rounded-md mt-0.5 font-sans pl-2 p-0.5 text-sm h-16"
+            placeholder="Escriba su mensaje aquí"
+          />
 
-        <button
-          type="submit"
-          className="rounded-md bg-azulmedio text-blanco mt-6  flex py-2 px-4 justify-center"
-        >
-          Enviar
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="rounded-md bg-azulmedio text-blanco mt-6  flex py-2 px-4 justify-center"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
